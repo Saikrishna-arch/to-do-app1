d41fd787fd6e62f4e57d1e9edc417de2aafceefd
Saikrishna-arch <70094594+Saikrishna-arch@users.noreply.github.com>
2021-03-22 19:33:19 +0530

    Add files via upload

---
+++ ToDo_App-boilerplate-main/app.js
@@ -0,0 +1,10 @@
+ var listOfActivities=[];
+
+ 
+ var input =document.getElementById("input");
+
+ var toDolist =document.getElementById("todolist");
+
+ document.getElementById("button").onclick=click;
+
+ function click()listOfActivities
